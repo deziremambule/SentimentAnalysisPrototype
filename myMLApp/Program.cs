02f10b5f101f244d9c1fe5599a52cf221f812419
Dezire Mambule <dmambule@glam.ac.uk>
2022-03-16 10:45:08 +0000

    Added Second Sentiment Analysis example.

---
+++ myMLApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyMLApp;
+// Add input data
+
+Console.WriteLine("Enter you text");
+
+
+string Usertext = Console.ReadLine();
+
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = Usertext
+
+};
+
+// Load model and predict output of sample data
+var result = SentimentModel.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+string sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
